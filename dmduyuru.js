d40faabc472cd66d4296ef5d786da6258f63d52e
WhisperM <51513397+WhisperM@users.noreply.github.com>
2019-06-08 20:11:43 +0300

    Add files via upload

---
+++ dmduyuru.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+exports.run = (client, message, args) => {
+if (message.author.id !=511544181974499357 ) { return; }
+  let mesaj = args.slice(0).join(' ');
+if (mesaj.length < 1) return message.channel.send('Birşey Yazmalısınız');
+  message.delete();
+      const mesajat = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setDescription('' + mesaj + '')
+
+      client.users.forEach(u => {
+u.sendEmbed(mesajat)
+})
+message.react("👨‍🏭")
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["dmduyuru","botduyuru"],
+  permLevel: 4
+};
+
+exports.help = {
+  name: 'dmduyuru',
+  description: 'İstediğiniz şeyi bota duyurtur.',
+  usage: 'duyuru [duyurmak istediğiniz şey]'
+};
+//XiR
